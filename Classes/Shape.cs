cc439cb420582285f4f539d5a23b37a5780053f7
JadenLi613 <jadenli613@outlook.com>
2025-02-17 18:09:56 +1300

    Shape.cs

--- Classes/Shape.cs
+++ Classes/Shape.cs
@@ -1,18 +1,9 @@
-﻿
 namespace Week1ObjectOriented.Classes
 {
-    public abstract class Shape
+    public abstract class Shape : IShape
     {
-        private int area;
-
-        public abstract int GetArea();
-       
-
-        public void SetArea(int Area)
-        {
-            area = Area;
-        }
-
         public string? Colour { get; set; }
+
+        public abstract double GetArea();
     }
 }
